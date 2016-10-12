a2050f3f64882a4ac95611031ff7ad71b421b873
Anne <anne.cognet@holbertonschool.com>
2016-10-12 19:12:07 +0000

    add question 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *second program in c
+ */
+
+/**
+ *main
+ *print a string followed by a new line using puts
+ *Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
