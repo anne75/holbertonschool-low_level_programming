fcf3116645808782de57afa441805aed220d43b1
Anne <anne.cognet@holbertonschool.com>
2016-10-12 19:18:06 +0000

    add question 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *after using puts in 4-..., see here use of printf
+ */
+
+/**
+ *main
+ *prints a string and new line using printf
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
