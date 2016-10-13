c9db70412cf22c8da73e842520c3253b5d47a7d3
Anne <anne.cognet@holbertonschool.com>
2016-10-13 20:23:47 +0000

    add question 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 /*
- *print the alphabet using only putchar
+ * print the alphabet using only putchar
  */
 
 /**
- *main - entry point
- *spits out the lower case alphabet, use putchar no more
- *than twice
- *Return: 0
+ * main - entry point
+ * spits out the lower case alphabet,  no more
+ * than twice
+ * Return: 0
  */
 int main(void)
 {
-	int c = 97;
+	int c;
 
-	while (c <= 122)
+	c = 'a';
+
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
