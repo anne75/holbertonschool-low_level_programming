dce7a752402fd985c3f776e5b573469e1840a35b
Anne <anne.cognet@holbertonschool.com>
2016-10-13 00:00:08 +0000

    add question 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ *prints to the standard error without using common functions.
+ */
+
+/**
+ *main - entry point
+ *writes a string to the standard error
+ *Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1);
+}
