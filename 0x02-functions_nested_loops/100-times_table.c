e711d28b5dca4a6206264a0e6a6a72e7729ae7b8
Anne <anne.cognet@holbertonschool.com>
2016-10-18 18:27:17 +0000

    add question 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,62 +8,49 @@
  */
 void print_times_table(int n)
 {
-	int a;
+	int a, b, p;
 
 	if (n >= 0 && n <= 15)
 	{
 		a = 0;
 		while (a <= n)
 		{
-			make_line(a, n);
+			b = 0;
+			while (b <= n)
+			{
+				if (b > 0)
+					_putchar(' ');
+				p = a * b;
+				if (p < 10)
+				{
+					if (b > 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + p);
+				}
+				else
+				{
+					if (p < 100 && b > 0)
+					{
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar('0' + p / 100);
+						p = p - (p / 100) * 100;
+					}
+					_putchar('0' + p / 10);
+					_putchar('0' + p % 10);
+				}
+				if ((a <  n && b == n) || (a == n && b == n))
+					_putchar('\n');
+				else
+					_putchar(',');
+				b++;
+			}
 			a++;
 		}
-	}
-}
-
-/**
- * make_line - helper function to print each row
- * @a: line we are making
- * @n: length of line
- * Return: void
- */
-void make_line(int a, int n)
-{
-	int b, p;
-
-	b = 0;
-	while (b <= n)
-	{
-		if (b > 0)
-			_putchar(' ');
-		p = a * b;
-		if (p < 10)
-		{
-			if (b > 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			_putchar('0' + p);
-		}
-		else
-		{
-			if (p < 100 && b > 0)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('0' + p / 100);
-				p = p - (p / 100) * 100;
-			}
-			_putchar('0' + p / 10);
-			_putchar('0' + p % 10);
-		}
-		if ((a <  n && b == n) || (a == n && b == n))
-			_putchar('\n');
-		else
-			_putchar(',');
-		b++;
 	}
 }
