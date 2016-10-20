5bb3ee0efee8a1d9b872262498423a9f2452c34a
Anne <anne.cognet@holbertonschool.com>
2016-10-20 23:58:33 +0000

    add question 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ * printc - print int c a times
+ * @a: number of times to print c must be > 0
+ * @c: character to print
+ * Return: void
+ */
+void printc(int a, int c)
+{
+	while (a > 0)
+	{
+		_putchar(c);
+		a--;
+	}
+}
+
+/**
  * print_triangle - print a triangle, ascii art
  * @size: size of triangle
  * Return: void
@@ -16,20 +31,5 @@ void print_triangle(int size)
 		printc(i, '#');
 		_putchar('\n');
 		i++;
-	}
-}
-
-/**
- * printc - print int c a times
- * @a: number of times to print c must be > 0
- * @c: character to print
- * Return: void
- */
-void printc(int a, int c)
-{
-	while (a > 0)
-	{
-		_putchar(c);
-		a--;
 	}
 }
