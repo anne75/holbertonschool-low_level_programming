31dc58199512060b819f9ec61e40a99e7334d0b6
Anne <anne.cognet@holbertonschool.com>
2016-10-20 23:17:24 +0000

    add question 8

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,20 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, a;
 
 	if (size > 0)
 	{
 		i = 0;
 		while (i < size)
 		{
-			printsq(size);
+			a = i;
+			while (a > 0)
+			{
+				_putchar('#');
+				a--;
+			}
+			_putchar('\n');
 			i++;
 		}
 	}
@@ -22,19 +28,4 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	}
-}
-
-/**
- * printsq - print one line of # of size i
- * @i: length of line, should be > 0
- * Return: void
- */
-void printsq(int i)
-{
-	while (i > 0)
-	{
-		_putchar('#');
-		i--;
-	}
-	_putchar('\n');
 }
