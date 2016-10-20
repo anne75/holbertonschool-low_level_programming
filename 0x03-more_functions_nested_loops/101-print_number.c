bb8050488d6dab14c5dc7d5446b1bf21dd6ab14f
Anne <anne.cognet@holbertonschool.com>
2016-10-20 23:48:37 +0000

    add question 12

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -13,13 +13,9 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if ((n / 10) == 0)
-	{
-		_putchar('0' + n);
-	}
-	else
-	{
+
+	if ((n / 10) > 0)
 		print_number(n / 10);
-		_putchar('0' + n % 10);
-	}
+
+	_putchar('0' + n % 10);
 }
