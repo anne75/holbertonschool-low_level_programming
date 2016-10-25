edb3ebc3936c2ab44a05936bd1650d562e88026c
Anne <anne.cognet@holbertonschool.com>
2016-10-25 16:49:49 +0000

    if n == 0

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,19 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-
-	while (i < (n - 1))
+	if (n == 0)
 	{
-		printf("%d, ", *(a + i));
-		i++;
+		printf("\n");
 	}
-	printf("%d\n", *(a + i));
+	else
+	{
+		i = 0;
+
+		while (i < (n - 1))
+		{
+			printf("%d, ", *(a + i));
+			i++;
+		}
+		printf("%d\n", *(a + i));
+	}
 }
