ae640f9da19c95d9a4ab69d6eaf3d930d4313b31
Anne <anne.cognet@holbertonschool.com>
2016-10-25 16:36:00 +0000

    change ;

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - takes a pointer to an int and updates value to 98
- *@n; pointer to an int, update value it points to
- *Return: void
+ * @n: pointer to an int, update value it points to
+ * Return: void
  */
 void reset_to_98(int *n)
 {
