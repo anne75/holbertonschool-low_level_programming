0f1fd19ba52d36e0d1429213b9d94b21f5c2a2af
Anne <anne.cognet@holbertonschool.com>
2016-10-27 19:36:12 +0000

    question 6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * sep - check if a char is a word separator
+ * @c: char to check
+ * Return 1 if true, 0 otherwise
+ */
+int sep(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t' ||
+	    c == ',' || c == ';' || c == '.' || c == '!' ||
+	    c == '?' || c == '"' || c == '(' || c == ')' ||
+	    c == '{' || c == '}')
+		return (1);
+	return (0);
+}
+
+/**
+ * _islower - check if char is lower case letter
+ * @c: character to check
+ * Return: 1 if true 0 if false
+ */
+int _islower(char c)
+{
+	if (c >= 'a' && c<= 'z')
+		return (1);
+	return (0);
+}
+
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		printf("CHAR %c, sep? %i, next %c\n", *(s + i), sep(*(s + i)), *(s + i + 1));
+		if (sep(*(s + i)) == 1 && *(s + i + 1) != '\0' &&
+			_islower(*(s + i + 1)) == 1)
+		{
+			*(s + i + 1) = *(s + i + 1) - 'a' + 'A';
+			i++;
+		}
+		i++;
+	}
+	return (s);
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,27 @@
+/*headers repo 0x05*/
+
+/*_putchar, ony printing function authorized*/
+int _putchar(char);
+
+/*question 0*/
+char *_strcat(char *dest, char *src);
+
+/*question 1*/
+char *_strncat(char *dest, char *src, int n);
+
+/*question 2*/
+char *_strncpy(char *dest, char *src, int n);
+
+/*question 3*/
+int _strcmp(char *s1, char *s2);
+
+/*question 4*/
+void reverse_array(int *a, int n);
+
+/*question 5*/
+char *string_toupper(char *);
+
+/*question 6*/
+int sep(char);
+int _islower(char);
+char *cap_string(char *);
