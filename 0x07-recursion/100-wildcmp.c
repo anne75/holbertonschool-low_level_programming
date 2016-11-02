c3a69cc0d96f28f5ccb211f3dff511af58fdba84
Anne <anne.cognet@holbertonschool.com>
2016-11-02 21:21:55 +0000

    add 100

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 /*I have no char in s1 matching the char in s2 after * */
 		s3  = _strchar(s1, *(s2 + 1);
-		if (s3) == NULL)
+		if (s3 == NULL)
 			return (0);
 /*I have 1 or more char in s1 matching the char in s2 */
 		c = wildcmp(s3, s2 + 1);
