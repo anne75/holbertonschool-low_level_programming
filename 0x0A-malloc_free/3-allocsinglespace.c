a0e2b9bf000e9bfae2a41ec40c6c415778ee128a
Anne <anne.cognet@holbertonschool.com>
2016-11-09 23:16:17 +0000

    playing with 3

---
+++ 0x0A-malloc_free/3-allocsinglespace.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * _memset - simple version of memset()
+ * @s: buffer to modify
+ * @b: value to modify buffer with
+ * @n: number of bytes pointed to by s to modify with char b
+ *
+ * Return: pointer to buffer, s
+ */
+int *_memset(int *s, int b, int n)
+{
+	int i;
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < n && (s + i) != NULL)
+		*(s + i++) = b;
+	if (s == NULL)
+		return (NULL);
+
+
+	return (s);
+}
+
+/**
+ * alloc_grid - return a pointer to a newly created 2 dim integer grid
+ * @width: width of grid or columns
+ * @height: height of grid or columns
+ * Return: pointer to grid on success, or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a, i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(height * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+	*a = malloc(sizeof(int) * width * height);
+	if (*a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	i = 0;
+	while (i <= height)
+	{
+		a[i] = (*a + width * i);
+		i++;
+	}
+
+	i = 0;
+	while (i < height)
+	{
+		a[i] = _memset(a[i], 0, width);
+		i++;
+	}
+	return (a);
+}
