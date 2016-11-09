5e6251d20926495adb3aea8006c94c42fb2b188c
Anne <anne.cognet@holbertonschool.com>
2016-11-09 18:08:10 +0000

    NULL strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * _strlen - I cannot use strlen()
  * @s: string
- * Return: return size of string 0 included
+ * Return: return size of string
  */
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-
-	while (*(s + i++) != '\0')
-		;
+	while (*(s + i) != '\0')
+		++i;
 	return (i);
 }
 
@@ -28,23 +27,26 @@ char *str_concat(char *s1, char *s2)
 	int l1, l2, i;
 	char *s;
 
-	if (s1 == NULL || s2 == NULL)
+	l1 = l2 = 0;
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	--l1;
-
-	s = malloc((l1 + l2) * sizeof(char));
+	if (s1 != NULL)
+		l1 = _strlen(s1);
+	if (s2 != NULL)
+		l2 = _strlen(s2);
+	printf("length %s %i, %s %i\n", s1, l1, s2, l2);
+	s = malloc((l1 + l2 + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
 	i = 0;
-	while (i < l1)
-		*(s + i++) = *s1++;
+	if (s1 != NULL)
+		while (i < l1)
+			*(s + i++) = *s1++;
 
-	while (i < (l1 + l2))
-		*(s + i++) = *s2++;
-
+	if (s2 != NULL)
+		while (i < (l1 + l2))
+			*(s + i++) = *s2++;
+	*(s + i) = '\0';
 	return (s);
 }
