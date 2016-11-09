b47047f7b6348c4ae313c42f91faf4916d979e29
Anne <anne.cognet@holbertonschool.com>
2016-11-09 08:05:30 +0000

    max size

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -3,6 +3,31 @@
 #include <limits.h>
 
 /**
+ * _memset - simple version of memset()
+ * @s: buffer to modify
+ * @b: value to modify buffer with
+ * @n: number of bytes pointed to by s to modify with char b
+ *
+ * Return: pointer to buffer, s
+ */
+int *_memset(int *s, int b, int n)
+{
+	int i;
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < n && (s + i) != NULL)
+		*(s + i++) = b;
+	if (s == NULL)
+		return (NULL);
+
+
+	return (s);
+}
+
+/**
  * alloc_grid - return a pointer to a newly created 2 dim integer grid
  * @width: width of grid or columns
  * @height: height of grid or columns
@@ -12,7 +37,7 @@ int **alloc_grid(int width, int height)
 {
 	int **a, i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0 || height * width > 4000000)
 		return (NULL);
 
 	a = malloc(height * sizeof(*a));
@@ -22,23 +47,30 @@ int **alloc_grid(int width, int height)
 	i = 0;
 	while (i < height)
 	{
+		printf("%i:%i\n ", i, i);
 		a[i] = malloc(width * sizeof(**a));
 		if (a[i] == NULL)
 		{
+			printf("here 2\n");
 			while (--i >= 0)
 				free(a[i]);
 			free(a);
 			return (NULL);
 		}
 		i++;
+		printf("there %i %i\n", i < height, a == NULL);
 	}
+	printf("there/n");
 	i = 0;
 	while (i < height)
 	{
 		j = 0;
 		while (j < width)
 		{
-			a[i][j] = 0;
+			if (a[i] != NULL)
+				a[i][j] = 0;
+			else
+				return (NULL);
 			j++;
 		}
 		i++;
