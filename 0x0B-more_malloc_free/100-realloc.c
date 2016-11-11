45dc3fff1d4dbc8155fd8f0228c7e88a5fdef595
Anne <anne.cognet@holbertonschool.com>
2016-11-11 01:47:09 +0000

    1

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -19,10 +19,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new;
 	char *help1, *help2;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
-		/*if (ptr == NULL)
-		  return (NULL);*/
 		free(ptr);
 		return (NULL);
 	}
