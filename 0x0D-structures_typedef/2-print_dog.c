870a623fb5f052188f4a19eeea3b41e71e712922
Anne <anne.cognet@holbertonschool.com>
2016-11-14 22:29:09 +0000

    try

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
-#define CHECK(x) ((x == NULL) ? "(nil)": (x))
+#define CHECK(x) ((x == NULL) ? "(nil)" : (x))
 
+/**
+ * print_dog - print dog
+ * @d: dog struct
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
 	printf("Name: %s\n", CHECK(d->name));
-	printf("Age: ");
-	if (d->age == 0)
-		puts("(nil)");
-	else
-		printf("%.2f\n", d->age);
+	printf("Age: %.1f\n", d->age);
 	printf("Owner: %s\n", CHECK(d->owner));
 }
