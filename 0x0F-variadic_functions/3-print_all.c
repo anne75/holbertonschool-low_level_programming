90645c9c86ed2f32ff4805cfeadddc6e7e452e13
Anne <anne.cognet@holbertonschool.com>
2016-11-20 00:37:29 +0000

    thanks all

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -5,41 +5,38 @@
  * print_ci - prints va if it is a char or an int
  * @v: va_list
  * @s: format for printf
- * @end: separator for printf
  * Return: void
  */
-void print_ci(va_list v, char *s, char *end)
+void print_ci(va_list v, char *s)
 {
-	printf(s, end, va_arg(v, int));
+	printf(s, va_arg(v, int));
 }
 
 /**
  * print_d - prints va if it is a float
  * @v: va_list
  * @s: format for printf
- * @end: separator for printf
  * Return: void
  */
-void print_d(va_list v, char *s, char *end)
+void print_d(va_list v, char *s)
 {
-	printf(s, end, va_arg(v, double));
+	printf(s, va_arg(v, double));
 }
 
 /**
  * print_s - prints va if it is a char *
  * @v: va_list
  * @s: format for printf
- * @end: separator for printf
  * Return: void
  */
-void print_s(va_list v, char *s, char *end)
+void print_s(va_list v, char *s)
 {
 	char *arg;
 
 	arg = va_arg(v, char *);
 	if (!arg)
 		arg = "(nil)";
-	printf(s, end, arg);
+	printf(s, arg);
 }
 
 /**
@@ -54,11 +51,10 @@ void print_all(const char * const format, ...)
 	char *end;
 	va_list alist;
 
-	fs array[] = {{print_ci, "%s%c", 'c'},
-		      {print_d, "%s%f", 'f'},
-		      {print_ci, "%s%i", 'i'},
-		      {print_s, "%s%s", 's'},
-		      {NULL, "", '\0'}
+	fs array[] = {{print_ci, "%c", 'c'},
+		      {print_d, "%f", 'f'},
+		      {print_ci, "%i", 'i'},
+		      {print_s, "%s", 's'},
 	};
 
 	va_start(alist, format);
@@ -67,11 +63,12 @@ void print_all(const char * const format, ...)
 	while (format && *(format + j))
 	{
 		i = 0;
-		while (i < 5)
+		while (i < 4)
 		{
 			if (*(format + j) == array[i].def)
 			{
-				array[i].mat(alist, array[i].s, end);
+				printf("%s", end);
+				array[i].mat(alist, array[i].s);
 			}
 			++i;
 		}
