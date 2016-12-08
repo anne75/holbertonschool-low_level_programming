7a5e1832c013154e1271656f0be306beb9d069d7
Anne <anne.cognet@holbertonschool.com>
2016-12-08 04:54:54 +0000

    ? not working

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -27,6 +27,11 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	nw = write(fd, text_content, l);
+	if (nw == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
 	if (nw == l)
 		return (1);
