c5694c0d43c27db983c1d4437b6a0ed29ac0c4cf
Anne <anne.cognet@holbertonschool.com>
2016-12-09 06:06:37 +0000

    even more edge cases

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,25 +14,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nr, nw;
 	char *buffer;
 
+	nw = 0;
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 
 	nr = read(fd, buffer, letters);
 	if (nr == -1)
 	{
 		free(buffer);
+		close(fd);
 		return (0);
 	}
-	nw = write(STDOUT_FILENO, buffer, nr);
+	if(nr != 0)
+		nw = write(STDOUT_FILENO, buffer, nr);
 	free(buffer);
 	if (close(fd) == -1 || nw == -1 || nr != nw)
 		return (0);
