46e9ac6a8ddfc9688d559c309249258666624d2d
Anne <anne.cognet@holbertonschool.com>
2017-02-02 04:55:55 +0000

    add null checks

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int hash;
 	hash_node_t *new;
 
-	if (!ht || !key || !value)
+	if (!ht || !key)
 		return (0);
 
 	hash = key_index((const unsigned char *)key, ht->size);
