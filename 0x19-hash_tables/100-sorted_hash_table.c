e086ba3f3a8a683e5180d548cf7041e19e9bf53e
Anne <anne.cognet@holbertonschool.com>
2017-02-03 20:32:26 +0000

    fixed once

--- 0x19-hash_tables/100-sorted_hash_table.c
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -33,6 +33,54 @@ shash_table_t *shash_table_create(unsigned long int size)
 }
 
 /**
+ * insert_sorted_node - insert a node in sorted hash order
+ * @ht: pointer to hash table
+ * @new: node
+ * Return: 1 on success, 0 on failure
+ */
+int insert_sorted_node(shash_table_t *ht, shash_node_t *new)
+{
+	unsigned long int hash;
+	shash_node_t *tmp;
+
+	hash = key_index((const unsigned char *)new->key, ht->size);
+
+	if (ht->shead == NULL)
+	{
+		ht->shead = new;
+		ht->stail = new;
+	}
+	else
+	{
+		if (hash < key_index((const unsigned char *)(ht->shead)->key,
+				     ht->size))
+		{
+			new->snext = ht->shead, (ht->shead)->sprev = new;
+			ht->shead = new;
+		}
+		else if (hash >
+			 key_index((const unsigned char *)(ht->stail)->key,
+				   ht->size))
+		{
+			(ht->stail)->snext = new, new->sprev = ht->stail;
+			ht->stail = new;
+		}
+		else
+		{
+/* should be able to put last 2 cases together, better ?*/
+			tmp = ht->shead;
+			while (tmp->snext && hash >
+			       key_index((const unsigned char *)tmp->snext->key,
+						ht->size))
+				tmp = tmp->snext;
+			new->sprev = tmp, new->snext = tmp->snext;
+			(tmp->snext)->sprev = new, tmp->snext = new;
+		}
+	}
+	return (1);
+}
+
+/**
  * shash_table_set - insert an element in a sorted hash table
  * @ht: pointer to hash table
  * @key: key value
@@ -42,7 +90,7 @@ shash_table_t *shash_table_create(unsigned long int size)
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash;
-	shash_node_t *new, *tmp;
+	shash_node_t *new;
 
 	if (!ht | !key)
 		return (0);
@@ -76,45 +124,69 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		new->next = ((ht->array)[hash]);
 		(ht->array)[hash] = new;
 	}
-	if (ht->shead == NULL)
-	{
-		ht->shead = new;
-		ht->stail = new;
-	}
-	else
-	{
-		if (hash < key_index((const unsigned char *)(ht->shead)->key,
-				     ht->size))
-		{
-			new->snext = ht->shead, (ht->shead)->sprev = new;
-			ht->shead = new;
-		}
-		else if (hash >
-			 key_index((const unsigned char *)(ht->stail)->key,
-				   ht->size))
-		{
-			(ht->stail)->snext = new, new->sprev = ht->stail;
-			ht->stail = new;
-		}
-		else
-		{
-			tmp = ht->shead;
-			while (hash > key_index((const unsigned char *)tmp->key,
-						ht->size))
-				tmp = tmp->snext;
-			new->sprev = tmp, new->snext = tmp->next;
-			(tmp->snext)->sprev = new, tmp->snext = new;
-		}
-	}
-	return (1);
+	return (insert_sorted_node(ht, new));
+}
+
+/**
+ * shash_table_get - retrieve value from key
+ * @ht: hash table
+ * @key: key
+ * Return: value, a string
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int hash;
+	shash_node_t *node;
+
+	if (!ht)
+		return (NULL);
+	hash = key_index((const unsigned char *)key, ht->size);
+
+	if ((ht->array)[hash] == NULL)
+		return (NULL);
+
+	node = (ht->array)[hash];
+	while (node && _strcmp(node->key, (char *)key))
+		node = node->next;
+
+	if (!node)
+		return (NULL);
+	return (node->value);
+
 }
 
 
-
-char *shash_table_get(const shash_table_t *ht, const char *key)
-{}
 void shash_table_print(const shash_table_t *ht)
-{}
-void shash_table_print_rev(const shash_table_t *ht){}
+{
+	shash_node_t *node;
+	char *flag = "";
 
+	node = ht->shead;
 
+	putchar('{');
+	while (node)
+	{
+		printf("%s'%s': '%s'", flag, node->key, node->value);
+		flag = ", ";
+		node = node->snext;
+	}
+	puts("}");
+
+}
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	char *flag = "";
+
+	node = ht->stail;
+
+	putchar('{');
+	while (node)
+	{
+		printf("%s'%s': '%s'", flag, node->key, node->value);
+		flag = ", ";
+		node = node->sprev;
+	}
+	puts("}");
+}
