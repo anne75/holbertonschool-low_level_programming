fa290e95c343a887287ab9c5415199f1aa811aba
Anne <anne.cognet@holbertonschool.com>
2017-02-04 21:59:55 +0000

    sort alphabetically

--- 0x19-hash_tables/100-sorted_hash_table.c
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -40,10 +40,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int insert_sorted_node(shash_table_t *ht, shash_node_t *new)
 {
-	unsigned long int hash;
 	shash_node_t *tmp;
-
-	hash = key_index((const unsigned char *)new->key, ht->size);
 
 	if (ht->shead == NULL)
 	{
@@ -52,15 +49,12 @@ int insert_sorted_node(shash_table_t *ht, shash_node_t *new)
 	}
 	else
 	{
-		if (hash < key_index((const unsigned char *)(ht->shead)->key,
-				     ht->size))
+		if (strcmp(new->key, (ht->shead)->key) <= 0)
 		{
 			new->snext = ht->shead, (ht->shead)->sprev = new;
 			ht->shead = new;
 		}
-		else if (hash >
-			 key_index((const unsigned char *)(ht->stail)->key,
-				   ht->size))
+		else if (strcmp(new->key, (ht->stail)->key) > 0)
 		{
 			(ht->stail)->snext = new, new->sprev = ht->stail;
 			ht->stail = new;
@@ -69,15 +63,62 @@ int insert_sorted_node(shash_table_t *ht, shash_node_t *new)
 		{
 /* should be able to put last 2 cases together, better ?*/
 			tmp = ht->shead;
-			while (tmp->snext && hash >
-			       key_index((const unsigned char *)tmp->snext->key,
-						ht->size))
+			while (tmp->snext && strcmp(new->key, (tmp->snext)->key) > 0)
 				tmp = tmp->snext;
 			new->sprev = tmp, new->snext = tmp->snext;
 			(tmp->snext)->sprev = new, tmp->snext = new;
 		}
 	}
 	return (1);
+}
+
+/**
+ * create_node - create a node
+ * @ht: hash table
+ * @key: a key
+ * @value: value
+ * @hash: index
+ * Return: 0 on failure, 1 on success
+ */
+int create_node(shash_table_t *ht, const char *key,
+		    const char *value, unsigned long int hash)
+{
+	shash_node_t *new;
+	char *s;
+
+	new = malloc(sizeof(shash_node_t));
+	if (!new)
+		return (0);
+	s = strdup(key);
+	if (!s)
+	{
+		free(new);
+		return (0);
+	}
+	new->key = s;
+	s = NULL;
+	if (value)
+	{
+		s = strdup(value);
+		if (!s)
+		{
+			free(new->key);
+			free(new);
+			return (0);
+		}
+	}
+	new->value = s;
+	new->next = NULL, new->sprev = NULL, new->snext = NULL;
+	if ((ht->array)[hash] == NULL)
+	{
+		(ht->array)[hash] = new;
+	}
+	else
+	{
+		new->next = ((ht->array)[hash]);
+		(ht->array)[hash] = new;
+	}
+	return (insert_sorted_node(ht, new));
 }
 
 /**
@@ -91,40 +132,29 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash;
 	shash_node_t *new;
+	char *s;
 
 	if (!ht | !key)
 		return (0);
 	hash = key_index((const unsigned char *)key, ht->size);
+	s = NULL;
 /*update node*/
 	new = (ht->array)[hash];
 	while (new && strcmp(new->key, (char *)key))
 		new = new->next;
 	if (new)
-		new->value = (char *)value;
-	new = ht->shead;
-	while (new && strcmp(new->key, (char *)key))
-		new = new->snext;
-	if (new)
 	{
-		new->value = (char *)value;
+		if (value)
+		{
+			s = strdup(value);
+			if (!s)
+				return (0);
+		}
+		new->value = s;
 		return (1);
 	}
 /*create new node*/
-	new = malloc(sizeof(shash_node_t));
-	if (!new)
-		return (0);
-	new->key = (char *)key, new->value = (char *)value;
-	new->next = NULL, new->sprev = NULL, new->snext = NULL;
-	if ((ht->array)[hash] == NULL)
-	{
-		(ht->array)[hash] = new;
-	}
-	else
-	{
-		new->next = ((ht->array)[hash]);
-		(ht->array)[hash] = new;
-	}
-	return (insert_sorted_node(ht, new));
+	return (create_node(ht, key, value, hash));
 }
 
 /**
@@ -196,6 +226,40 @@ void shash_table_print_rev(const shash_table_t *ht)
 		printf("%s'%s': '%s'", flag, node->key, node->value);
 		flag = ", ";
 		node = node->sprev;
+	}
+	puts("}");
+}
+
+
+
+/**
+ * hash_table_printt - print a hash table
+ * @ht: a hash table
+ */
+void hash_table_printt(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	unsigned long int i;
+	char *flag = "";
+
+	if (!ht)
+		return;
+
+	putchar('{');
+	for (i = 0; i < ht->size; ++i)
+	{
+		node = (ht->array[i]);
+		if (node != NULL)
+		{
+			puts("new node");
+			while (node)
+			{
+				printf("%s'%s': '%s'",
+				      flag, node->key, node->value);
+				flag = ", ";
+				node = node->next;
+			}
+		}
 	}
 	puts("}");
 }
