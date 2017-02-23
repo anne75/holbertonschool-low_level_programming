743ce53729364d2fd3e67068c5034c964ee6d30b
Anne <anne.cognet@holbertonschool.com>
2017-02-23 20:39:05 +0000

    comment selection sort

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -1,12 +1,17 @@
 #include "sort.h"
 
 /**
- *
+ * selection_sort - sort an array with selection sort
+ * @array: array of int
+ * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
 	int swap_in;
+
+	if (!array || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
