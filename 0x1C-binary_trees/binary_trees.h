af9f701a0a762db1f50fec10848551ef0af44c0b
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-06 23:57:29 +0000

    updating all tests and 16

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -86,5 +86,8 @@ int binary_tree_balance(const binary_tree_t *tree);
 /*question 15*/
 int binary_tree_is_full(const binary_tree_t *tree);
 
+/*question 16*/
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
 #endif
 
