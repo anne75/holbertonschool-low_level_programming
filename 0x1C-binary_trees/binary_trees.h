fb72389849ecfffc551be865dd1d4e16e5e8bcc4
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-06 20:22:38 +0000

    adding 12 and updating related files

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -74,7 +74,11 @@ size_t binary_tree_depth(const binary_tree_t *node);
 /*question 11*/
 size_t binary_tree_size(const binary_tree_t *tree);
 
+/*question 12*/
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
+/*question 13*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
 #endif
 
