cb4b8a67abc5bddce1183f7507384b95947d7676
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-06 19:56:55 +0000

    added root check and removed helper function in favor of macro

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -2,21 +2,6 @@
 
 #define MAX(X, Y) ((X) >= (Y) ? (X) : (Y))
 
-
-/**
- * _max - maximum between two int
- * @i: an int
- * @j: another int
- * Should I make it a macro ?
- * Return: the maximum value
- */
-int _max(int i, int j)
-{
-	(i >= j) ? return (i) : return (j);
-}
-
-
-
 /**
  * binary_tree_height - measure the height of a binary tree
  * @tree: root of binary tree
@@ -27,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t count;
 
 	count = 0;
-	if (tree)
+	if (tree && (tree->left || tree->right))
 		count = 1 + MAX(binary_tree_height(tree->left),
 				binary_tree_height(tree->right));
 
