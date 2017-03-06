fe44f8efff0fa3b7310a243266f4bfad4dbb3f26
Anne <anne.cognet@holbertonschool.com>
2017-03-06 19:38:23 +0000

    fix number 3

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -48,7 +48,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 /*question 3*/
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 /*question 4*/
 int binary_tree_is_leaf(const binary_tree_t *node);
