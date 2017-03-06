eab1a6a285cbaf675200c2e4a5ec8a414c32189d
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-06 19:44:14 +0000

    adding function pointers to recursive calls

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -11,8 +11,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree)
 	{
-		binary_tree_preorder(tree->left);
-		binary_tree_preorder(tree->right);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 		func(tree->n);
 	}
 }
