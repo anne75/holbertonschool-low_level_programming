957c6eb62d022ba6347964d3ff3afaa9da6be90e
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-06 21:39:09 +0000

    first draft of 14

---
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+#define MAX(X, Y) ((X) >= (Y) ? (X) : (Y))
+
+/**
+ * binary_tree_h - measure the height of a binary tree by nodecount
+ * @tree: root of binary tree
+ * Return: maximal number of nodes between the root and a leaf node
+ */
+int binary_tree_h(const binary_tree_t *tree)
+{
+	int count;
+
+	count = 0;
+	if (tree)
+		count = 1 + MAX(binary_tree_h(tree->left),
+				binary_tree_h(tree->right));
+	return (count);
+}
+/**
+ * binary_tree_balance - returns the difference of the maximal node depth on
+ * left and maximal node depth on right
+ * @tree: ptr to root of tree
+ * Return: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_h(tree->left) -
+		binary_tree_h(tree->right));
+
+}
