06ad5bd879b13b0ac1f10c503c6d8ca3bad96d78
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-20 17:16:43 +0000

    updating comments and base cases

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -2,23 +2,24 @@
 
 
 /**
- * binary_tree_h - measure the height of a binary tree by nodecount
+ * binary_tree_h - measure the height of a binary tree by edge count
  * @tree: root of binary tree
- * Return: maximal number of nodes between the root and a leaf node
+ * base case is -1 for NULL node
+ * Return: maximal number of edges on left or right
  */
 int binary_tree_h(const binary_tree_t *tree)
 {
 	int count;
 
-	count = 0;
+	count = -1;
 	if (tree)
 		count = 1 + MAX(binary_tree_h(tree->left),
 				binary_tree_h(tree->right));
 	return (count);
 }
 /**
- * binary_tree_balance - returns the difference of the maximal node depth on
- * left and maximal node depth on right
+ * binary_tree_balance - returns the difference of the maximal edge count on
+ * left and  maximal edge count on right
  * @tree: ptr to root of tree
  * Return: balance factor
  */
