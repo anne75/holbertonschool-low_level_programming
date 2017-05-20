3c97722ea2e5c444c71d42196d3cafac9eceb271
Anne <anne.cognet@holbertonschool.com>
2017-05-20 05:44:43 +0000

    betty

--- 0x1E-huffman_rb_trees/rb_trees.h
+++ 0x1E-huffman_rb_trees/rb_trees.h
@@ -9,12 +9,13 @@
  *
  * @RED: 0 -> Red node
  * @BLACK: 1 -> Black node
+ * @DOUBLE_BLACK: 2 -> Double black
  */
 typedef enum rb_color_e
 {
 	RED = 0,
 	BLACK,
-    DOUBLE_BLACK
+	DOUBLE_BLACK
 } rb_color_t;
 
 /**
